766e291587f2680c68680956c850a744388ff95e
matt <matthewlee821@gmailcom>
2025-03-14 16:21:47 +0100

    update

--- R/imputation.R
+++ R/imputation.R
@@ -6,7 +6,7 @@
 #'
 #' @param data your data variable e.g., mtcars
 #' @param output_dir where you want to save the output
-#' @param root_dir where you want to generate the report from
+#' @param knit_root_dir where you want to generate the report from
 #' @param subtitle what you want to call this imputation test
 #'
 #' @return The result of the imputation test.
@@ -14,22 +14,40 @@
 #' @examples
 #' \dontrun{
 #' imputation_test(data = mtcars,
-#'                 output_dir = "project1/data/",
-#'                 root_dir = "project1/",
-#'                 subtitle = "imputation testing for mtcars")
+#'                   output_dir = "project1/data/",
+#'                   knit_root_dir = "project1/",
+#'                   subtitle = "imputation testing for mtcars")
 #' }
+#'
+#' @importFrom rmarkdown render
+#' @importFrom ggplot2 ggplot aes geom_col labs scale_y_continuous element_text theme
+#' @importFrom cowplot theme_cowplot plot_grid
+#' @importFrom dplyr summarise across everything arrange mutate filter select group_by ungroup left_join mutate_all
+#' @importFrom tidyr pivot_longer gather
+#' @importFrom kableExtra kbl kable_styling scroll_box add_header_above
+#' @importFrom stats cor.test
+#' @importFrom utils head
+#' @importFrom tibble column_to_rownames rownames_to_column
+#' @importFrom scales percent_format
+#' @importFrom patchwork plot_layout
+#' @importFrom parallel makeCluster stopCluster
+#' @importFrom doParallel registerDoParallel
+#' @importFrom imputeLCMD impute.MAR.MNAR model.Selector
+#' @importFrom impute impute.knn
+#' @importFrom missForest missForest
+#' @importFrom pcaMethods pca completeObs
+#' @importFrom missMethods impute_median impute_mean
 #'
 #' @export
 imputation_test <- function(
     data,
     output_dir,
-    root_dir,
+    knit_root_dir,
     subtitle) {
-
+  
   rmarkdown::render(
     input = paste0(system.file(package = "ImputationReport"), "/rmd/imputation.qmd"),
     output_format = "html_document",
     output_dir = output_dir,
-    root_dir = root_dir,
     params = list(data = data, subtitle = subtitle))
 }
